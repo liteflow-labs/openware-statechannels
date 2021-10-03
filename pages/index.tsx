45bbe3cb2f1fc213bf6d96bd40961b9c23517bfa
Nicolas Mahe <nicolas@mahe.me>
2021-10-03 16:46:10 +0700

    add metamask connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
+import { useWeb3React } from '@web3-react/core'
 import Head from 'next/head'
+import { useEffect } from 'react'
+import { injectedConnector } from '../lib/connector'
 
 export default function Home(): JSX.Element {
+  const { account, activate, error: web3Error } = useWeb3React()
+
+  useEffect(() => {
+    if (web3Error) throw web3Error
+  }, [web3Error])
+
   return (
     <>
       <Head>
-        <title>My first DApp</title>
+        <title>OpenWare StateChannels POC</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello world</main>
+      <main>
+        <h1>OpenWare StateChannels POC</h1>
+
+        <div>
+          <h2>Wallet</h2>
+          <p>{account ? 'Connected with ' + account : 'Not connected'}</p>
+          {!account && (
+            <button
+              type="button"
+              onClick={() => {
+                void activate(injectedConnector, undefined, true)
+              }}
+              style={{ cursor: 'pointer' }}
+            >
+              Connect with Metamask
+            </button>
+          )}
+        </div>
+      </main>
     </>
   )
 }
