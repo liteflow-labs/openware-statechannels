5c68e8ea6e14dac689285136c4bc85ffe201d812
Nicolas Mahe <nicolas@mahe.me>
2021-10-07 16:50:22 +0700

    implement sign using ethers signer in flow 2

---
+++ packages/contracts/scripts/flow-2-bis.ts
@@ -0,0 +1,101 @@
+//
+// Flow 2
+// 2 states. all participants sign the last one
+// sign using custom signature functions without access to private key
+//
+
+import { arrayify, splitSignature } from '@ethersproject/bytes'
+import { AddressZero, HashZero } from '@ethersproject/constants'
+import {
+  Channel,
+  ContractArtifacts,
+  getFixedPart,
+  hashAppPart,
+  hashOutcome,
+  hashState,
+  State,
+} from '@statechannels/nitro-protocol'
+import { ethers } from 'hardhat'
+import { NitroAdjudicator } from '../types'
+
+async function main() {
+  // Deploying smart contract
+  console.log('Deploying smart contracts...')
+
+  // NitroAdjudicator
+  const NitroAdjudicator = await ethers.getContractFactory(
+    ContractArtifacts.NitroAdjudicatorArtifact.abi,
+    ContractArtifacts.NitroAdjudicatorArtifact.bytecode,
+  )
+  const nitroAdjudicator = (await NitroAdjudicator.deploy()) as NitroAdjudicator
+  await nitroAdjudicator.deployed()
+
+  // participants
+  const signers = (await ethers.getSigners()).slice(-3)
+  const participants = signers.map((s) => s.address)
+  console.log('Participants', participants.join(', '))
+
+  // Construct a final state
+  const chainId = (await ethers.provider.getNetwork()).chainId.toString()
+  const channelNonce = 0
+  const channel: Channel = { chainId, channelNonce, participants }
+  const largestTurnNum = 4
+
+  const states: State[] = [
+    {
+      isFinal: false,
+      channel,
+      outcome: [],
+      appDefinition: AddressZero,
+      appData: HashZero,
+      challengeDuration: 86400, // 1 day
+      turnNum: largestTurnNum - 1,
+    },
+    {
+      isFinal: true,
+      channel,
+      outcome: [],
+      appDefinition: AddressZero,
+      appData: HashZero,
+      challengeDuration: 86400, // 1 day
+      turnNum: largestTurnNum,
+    },
+  ]
+
+  // Generate a finalization proof
+  console.log('Signing...')
+  const whoSignedWhat = [1, 1, 1] // everyone sign the last state (index is 1)
+  const stateHashes = states.map((s) => hashState(s))
+  const signatures = await Promise.all(
+    signers.map(async (s, i) => {
+      return splitSignature(
+        await s.signMessage(arrayify(stateHashes[whoSignedWhat[i]])),
+      )
+    }),
+  )
+
+  // Call conclude
+  console.log('Concluding...')
+  const numStates = states.length
+  const fixedPart = getFixedPart(states[0])
+  const appPartHash = hashAppPart(states[0])
+  const outcomeHash = hashOutcome(states[1].outcome)
+  const tx = await nitroAdjudicator.conclude(
+    largestTurnNum,
+    fixedPart,
+    appPartHash,
+    outcomeHash,
+    numStates,
+    whoSignedWhat,
+    signatures,
+  )
+  await tx.wait()
+}
+
+main()
+  .then(() => {
+    console.log('Done')
+  })
+  .catch((error) => {
+    throw error
+  })
