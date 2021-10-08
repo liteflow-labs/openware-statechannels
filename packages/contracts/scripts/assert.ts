880b79c737655b35fa6f956621460fe26cfb6c5b
Nicolas Mahe <nicolas@mahe.me>
2021-10-08 16:50:36 +0700

    improve assert helper

--- packages/contracts/scripts/assert.ts
+++ packages/contracts/scripts/assert.ts
@@ -1,4 +1,5 @@
 import { Channel, ChannelMode } from '@statechannels/nitro-protocol'
+import { ethers } from 'hardhat'
 import { NitroAdjudicator } from '../types'
 import { fetchChannelMode } from './utils'
 
@@ -11,5 +12,17 @@ export async function assertChannelMode(
   const mode = await fetchChannelMode(nitroAdjudicator, channel)
   console.log('Channel status is:', mode)
   if (mode !== expectedMode)
-    throw new Error(`Channel expected to be in ${expectedMode} mode`)
+    throw new Error(
+      `Channel expected to be in ${expectedMode} mode. Currently in ${mode}.`,
+    )
+}
+
+export async function assertBalance(address: string, expectedBalance: string) {
+  const balance = (await ethers.provider.getBalance(address))
+    .toString()
+    .slice(0, expectedBalance.length)
+  if (balance !== expectedBalance)
+    throw new Error(
+      `incorrect balance. got ${balance}. expected ${expectedBalance}`,
+    )
 }
