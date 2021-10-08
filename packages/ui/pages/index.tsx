8e6a771b3b6d39d78b698082d6e7b59cf48b1bc2
Nicolas Mahe <nicolas@mahe.me>
2021-10-08 16:52:26 +0700

    wip challenge in UI

--- packages/ui/pages/index.tsx
+++ packages/ui/pages/index.tsx
@@ -10,9 +10,11 @@ import {
   getChannelId,
   getFixedPart,
   getStateSignerAddress,
+  getVariablePart,
   hashAppPart,
   hashOutcome,
   hashState,
+  signChallengeMessage,
   State,
 } from '@statechannels/nitro-protocol'
 import { abi as NitroAdjudicatorContractAbi } from '@statechannels/nitro-protocol/lib/artifacts/contracts/NitroAdjudicator.sol/NitroAdjudicator.json'
@@ -142,7 +144,7 @@ export default function Home(): JSX.Element {
               ],
               appDefinition: TrivialAppContractAddress,
               appData: HashZero,
-              challengeDuration: 86400, // 1 day
+              challengeDuration: 600, // 10 min
               turnNum: 0,
             },
           )
@@ -358,6 +360,66 @@ export default function Home(): JSX.Element {
     console.log('conclude tx is done')
   }, [account, channel, library, nitroAdjudicatorContract, state])
 
+  const challenge = useCallback(async () => {
+    if (!channel) throw new Error('channel is falsy')
+    if (!nitroAdjudicatorContract)
+      throw new Error('nitroAdjudicatorContract is falsy')
+    if (!account) throw new Error('account is falsy')
+    if (!library) throw new Error('library is falsy')
+    if (!state) throw new Error('state is falsy')
+
+    const largestTurnNum = state.turnNum
+    const whoSignedWhat = [0, 1]
+
+    // ask and verify signature
+    const sigs = state.channel.participants.map((participant) => {
+      const sign = window.prompt(`Enter signature of ${participant}`)
+      if (!sign) throw new Error('sign is falsy')
+      const signSplit = splitSignature(sign)
+      const signVerif = getStateSignerAddress({
+        state,
+        signature: signSplit,
+      })
+      if (participant.toLowerCase() !== signVerif.toLowerCase())
+        throw new Error('signature is from the wrong address signature')
+      console.log('signature is valid')
+      return signSplit
+    })
+
+    // challenge
+    const signer = library.getSigner(account)
+    // const challengeHash = hashChallengeMessage(state)
+    // const signingKey = new SigningKey(
+    //   '0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e',
+    // )
+    // const signedMessage = signingKey.signDigest(hashMessage(challengeHash)) // FIXME: this signature is not working with signMessage from wallet...
+    // const challengeSignature = splitSignature(signedMessage)
+    // https://github.com/statechannels/statechannels/blob/%40statechannels/nitro-protocol%400.17.3/packages/nitro-protocol/src/signatures.ts
+    const challengeSignature = signChallengeMessage(
+      [
+        { state, signature: sigs[0] },
+        { state, signature: sigs[0] },
+      ],
+      '0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e',
+    )
+
+    const fixedPart = getFixedPart(state)
+    const challengeTx = await nitroAdjudicatorContract
+      .connect(signer)
+      .challenge(
+        fixedPart,
+        largestTurnNum,
+        [getVariablePart(state), getVariablePart(state)],
+        0, // isFinalCount
+        sigs,
+        whoSignedWhat,
+        challengeSignature,
+      )
+    console.log('waiting for challenge tx', challengeTx.hash)
+    await challengeTx.wait()
+    console.log('challenge tx is done')
+  }, [account, channel, library, nitroAdjudicatorContract, state])
+
   const withdraw = useCallback(async () => {
     if (!nitroAdjudicatorContract)
       throw new Error('nitroAdjudicatorContract is falsy')
@@ -520,6 +582,13 @@ export default function Home(): JSX.Element {
               style={{ cursor: 'pointer' }}
             >
               Conclude
+            </button>{' '}
+            <button
+              type="button"
+              onClick={() => challenge()}
+              style={{ cursor: 'pointer' }}
+            >
+              Challenge
             </button>{' '}
             <button
               type="button"
